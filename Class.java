1dc8e1fefbcdb16659d5e2de0bff835bb0ae1e16
fosslab <testing@gmail.com>
2026-01-12 11:39:08 +0530

    First commit

---
+++ Class.java
@@ -0,0 +1,8 @@
+class Class
+{
+public static void main(String args[]){
+System.out.println("Git-VCS");
+System.out.println("This JavaProgram is working");
+}	
+}	
+
---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo
+{
+	public static void main(String[] args){
+		System.out.println("Git-VCS");
+		System.out.println("This JavaProgram is working");
+	}
+}	
